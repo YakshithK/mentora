39b6e6dd3381936aeba8818600d697f92a72f0a9
Hemit Patel <hemitvpatel@gmail.com>
2025-05-26 19:30:55 -0400

    the auth is FINALLY working

--- platform/src/lib/auth/index.ts
+++ platform/src/lib/auth/index.ts
@@ -1,5 +1,10 @@
 import { betterAuth } from "better-auth";
- 
+import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+
+const client = new MongoClient(process.env.MONGO_URL as string);
+const db = client.db();
+
 export const auth = betterAuth({
     socialProviders: { 
         google: { 
@@ -7,4 +12,5 @@ export const auth = betterAuth({
            clientSecret: process.env.GITHUB_CLIENT_SECRET as string, 
         }, 
     },
+    database: mongodbAdapter(db),
 })
