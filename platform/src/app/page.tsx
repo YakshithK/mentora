082305912358955e7b19acff2928aa70fe8cc193
Hemit Patel <hemitvpatel@gmail.com>
2025-05-26 22:03:53 -0400

    added redirection to dashboard if logged in

--- platform/src/app/page.tsx
+++ platform/src/app/page.tsx
@@ -1,7 +1,18 @@
 import LandingPage from '@/components/LandingPage'
+import { auth } from '@/lib/auth'
+import { redirect } from 'next/navigation'
+import { headers } from 'next/headers'
 import React from 'react'
 
-const HomePage = () => {
+const HomePage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(), // Use Next.js headers() function
+  })
+
+  if (session) {
+    redirect('/dashboard')
+  }
+
   return (
     <LandingPage />
   )
