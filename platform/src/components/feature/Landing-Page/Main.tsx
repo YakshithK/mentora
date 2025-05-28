644c3da4ea19736f03799e1fa2092fcc9adfc6e1
Hemit Patel <hemitvpatel@gmail.com>
2025-05-27 21:10:27 -0400

    added footer

--- platform/src/components/feature/Landing-Page/Main.tsx
+++ platform/src/components/feature/Landing-Page/Main.tsx
@@ -3,6 +3,7 @@ import MainHero from './MainHero';
 import BodyDescription from './BodyDescription';
 import Details from './Details';
 import Testimonials from './Testimonials';
+import Footer from './Footer';
 
 const LandingPage = () => {
   return (
@@ -11,6 +12,7 @@ const LandingPage = () => {
       <BodyDescription />
       <Details />
       <Testimonials />
+      <Footer />
     </>
   );
 };
