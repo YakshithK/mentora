6854818271dd2ec73b3efb62abac9de56f89c4a2
Hemit Patel <hemitvpatel@gmail.com>
2025-05-28 18:47:59 -0400

    changed theme to purple!!

--- platform/src/data/prompt/index.ts
+++ platform/src/data/prompt/index.ts
@@ -1,3 +1,4 @@
+import { PromptData } from '@/types/prompt'
 import { BrainCircuit, Map, BookMarked, Bot } from 'lucide-react'
 
 const brainIcon = BrainCircuit
@@ -5,28 +6,24 @@ const mapIcon = Map
 const bookIcon = BookMarked
 const botIcon = Bot
 
-export const promptData = [
+export const promptData: PromptData[] = [
   {
     icon: brainIcon,
-    iconColor: "blue",
     boldedText: "AP Learning Game",
     greyedText: "Make studying fun and interactive",
   },
   {
     icon: mapIcon,
-    iconColor: "orange",
     boldedText: "Harvard Admission",
     greyedText: "Harvard Medical's capstone project?"
   },
   {
     icon: bookIcon,
-    iconColor: "pink",
     boldedText: "Scholarship Finder",
     greyedText: "What are some aid opportunities"
   },
   {
     icon: botIcon,
-    iconColor: "purple",
     boldedText: "Counsellor",
     greyedText: "Internship tips and tricks"
   }
