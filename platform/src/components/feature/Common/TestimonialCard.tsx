40b0e320be1745d2bba4497141c7d8060d9ce3cb
Hemit Patel <hemitvpatel@gmail.com>
2025-05-27 20:58:27 -0400

    added header for testiimonial

---
+++ platform/src/components/feature/Common/TestimonialCard.tsx
@@ -0,0 +1,39 @@
+import { TestimonialCardProps } from '@/types/testimonial';
+
+const TestimonialCard: React.FC<TestimonialCardProps> = ({ text, name, date, rotate }) => {
+  // Determine rotation class based on the "rotate" prop
+  const rotationClass =
+    rotate === "yes-left"
+      ? "-rotate-6"
+      : rotate === "yes-right"
+      ? "rotate-6"
+      : ""; // No rotation for "no" or undefined
+
+  return (
+    <div 
+      className={`bg-purple-200 relative p-6 rounded-lg w-96 h-96 flex flex-col justify-center items-center text-center ${rotationClass}`}
+    >
+      {/* Sticky note effect */}
+      <div className="absolute top-0 -right-0.5 transform">
+        <img 
+          src={`/images/post-it-tip.png`}
+          alt="Sticky note corner"
+          className="h-12 w-12"
+        />
+      </div>
+
+      {/* Review text */}
+      <p className="mb-4 text-gray-800">
+        {text}
+      </p>
+
+      {/* Reviewer details */}
+      <div className="absolute bottom-4 left-6">
+        <p className="font-bold text-gray-800">{name}</p>
+        <p className="text-sm text-gray-600">{date}</p>
+      </div>
+    </div>
+  );
+};
+
+export default TestimonialCard;
--- platform/src/components/feature/Landing-Page/Testimonials.tsx
+++ platform/src/components/feature/Landing-Page/Testimonials.tsx
@@ -1,46 +1,18 @@
 import {testimonials} from '@/data/testimonial'
-import { TestimonialCardProps } from '@/types/testimonial';
-
-const TestimonialCard: React.FC<TestimonialCardProps> = ({ text, name, date, rotate }) => {
-  // Determine rotation class based on the "rotate" prop
-  const rotationClass =
-    rotate === "yes-left"
-      ? "-rotate-6"
-      : rotate === "yes-right"
-      ? "rotate-6"
-      : ""; // No rotation for "no" or undefined
-
-  return (
-    <div 
-      className={`bg-purple-200 relative p-6 rounded-lg w-96 h-96 flex flex-col justify-center items-center text-center ${rotationClass}`}
-    >
-      {/* Sticky note effect */}
-      <div className="absolute top-0 -right-0.5 transform">
-        <img 
-          src={`/images/post-it-tip.png`}
-          alt="Sticky note corner"
-          className="h-12 w-12"
-        />
-      </div>
-
-      {/* Review text */}
-      <p className="mb-4 text-gray-800">
-        {text}
-      </p>
-
-      {/* Reviewer details */}
-      <div className="absolute bottom-4 left-6">
-        <p className="font-bold text-gray-800">{name}</p>
-        <p className="text-sm text-gray-600">{date}</p>
-      </div>
-    </div>
-  );
-};
+import TestimonialCard from '@/components/feature/Common/TestimonialCard';
 
 const Testimonial = () => {
 
   return (
     <div className="mt-16 text-center">
+      <div className="mx-auto px-4 sm:px-6 lg:px-8 lg:mb-20">
+        <h2 className="text-base font-semibold leading-7 text-indigo-600 cursor-text">
+            Trusted by professionals and students
+        </h2>
+        <h2 className="mt-2 text-3xl font-manrope font-semibold tracking-tight text-gray-900 sm:text-5xl cursor-text mb-8">
+            Not convinced? Listen to students <i>like you</i>.
+        </h2>
+      </div>
 
       <div className="flex flex-wrap justify-center gap-8 mt-8 lg:space-x-12">
         {testimonials.map((testimonial, index) => (
