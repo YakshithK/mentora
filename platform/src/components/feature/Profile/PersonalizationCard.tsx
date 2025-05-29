941aea308e3987eff3d8d8bf0512da1ccf242568
Hemit Patel <hemitvpatel@gmail.com>
2025-05-29 17:10:32 -0400

    formatted properly

--- platform/src/components/feature/Profile/PersonalizationCard.tsx
+++ platform/src/components/feature/Profile/PersonalizationCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 interface PersonalizationCardProps {
     type: string;
@@ -7,20 +7,25 @@ interface PersonalizationCardProps {
     icon: React.ReactNode;
 }
 
-const PersonalizationCard:React.FC<PersonalizationCardProps> = ({type, description, icon, amountOfOccurance}) => {
-  return (
-                <div className="relative pl-16">
-              <dt className="text-base font-semibold leading-7 text-gray-900">
+const PersonalizationCard: React.FC<PersonalizationCardProps> = ({
+    type,
+    description,
+    icon,
+    amountOfOccurance,
+}) => {
+    return (
+        <div className="relative pl-16">
+            <dt className="text-base font-semibold leading-7 text-gray-900">
                 <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-purple-600">
                     <span className="text-white text-2xl">{icon}</span>
                 </div>
                 {type} ({amountOfOccurance})
-              </dt>
-              <dd className="mt-2 text-base leading-7 text-gray-600 cursor-text">
+            </dt>
+            <dd className="mt-2 text-base leading-7 text-gray-600 cursor-text">
                 {description}
-              </dd>
-            </div>
-  )
-}
+            </dd>
+        </div>
+    );
+};
 
-export default PersonalizationCard
+export default PersonalizationCard;
